58d4f30f262c405239b23d2f06e1341751c70fff
abdallah <abddoullah.bouzhar@gmail.com>
2023-11-12 13:53:14 +0100

    QUIZ-32

---
+++ demo/src/main/java/bouzhar/quiz/demo/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package bouzhar.quiz.demo.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
