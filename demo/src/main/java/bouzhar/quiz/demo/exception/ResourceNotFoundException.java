b1225a3e6c3899f59b69e8fd38ff4ec656114be1
abdallah <abddoullah.bouzhar@gmail.com>
2023-11-12 13:53:23 +0100

    Quiz-31

---
+++ demo/src/main/java/bouzhar/quiz/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package bouzhar.quiz.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
