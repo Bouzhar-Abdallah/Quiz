f40cb532d24a0e06f999f4e972d577081bab5b1b
abdallah <abddoullah.bouzhar@gmail.com>
2023-11-12 03:34:04 +0100

    QUIZ-12

---
+++ demo/src/main/java/bouzhar/quiz/demo/subject/SubjectService.java
@@ -0,0 +1,24 @@
+package bouzhar.quiz.demo.subject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubjectService {
+    private final SubjectRepository subjectRepository;
+
+    @Autowired
+    public SubjectService(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjectRepository.findAll();
+    }
+
+    public Subject addSubject(Subject subject) {
+        return subjectRepository.save(subject);
+    }
+}
