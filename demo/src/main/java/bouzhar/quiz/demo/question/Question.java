ab9d2968bcde1521b132c1cd6821f779a632d0f1
abdallah <abddoullah.bouzhar@gmail.com>
2023-11-12 13:33:16 +0100

    QUIZ-17

---
+++ demo/src/main/java/bouzhar/quiz/demo/question/Question.java
@@ -0,0 +1,58 @@
+package bouzhar.quiz.demo.question;
+
+import bouzhar.quiz.demo.level.Level;
+import bouzhar.quiz.demo.subject.Subject;
+import bouzhar.quiz.demo.validation.Validation;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Question implements Serializable{
+
+
+    @Id
+/*    @SequenceGenerator(
+            name ="question_sequence",
+            sequenceName ="question_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "question_sequence"
+    )*/
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NonNull private Integer answersCount;
+    @NonNull private Integer correctAnsqersCount;
+    @NonNull private String text;
+    @NonNull private BigInteger duration;
+    @NonNull private Float scorePoints;
+    @Enumerated(EnumType.STRING)
+    @NonNull private QuestionType type;
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Validation> validations;
+
+
+    @ManyToOne()
+    @JoinColumn(name = "level_id")
+    private Level level;
+
+
+    @ManyToOne()
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
+}
